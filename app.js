e7f9794e8dc74984dfae4382ed66913774fd4b09
ttyagi3963 <tarun.tyagi1978@gmail.com>
2019-06-05 18:43:01 -0700

    async wait

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ const path = require('path')
 const express = require("express");
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-
+const multer = require('multer');
+const uuidv4 = require('uuid/v4')
 
 const feedRoutes = require('./routes/feed')
+const authRoutes = require('./routes/auth')
 
 const MONGODB_URI =
   'mongodb+srv://ttyagi:123wsx@nodepracticemax-hjdhm.mongodb.net/messages';
@@ -14,8 +16,28 @@ const MONGODB_URI =
 
 const app = express();
 
+const fileStorage = multer.diskStorage({
+    destination: (req, file, cb) =>{
+        cb(null, 'images')
+    },
+    filename: (req, file, cb) =>{
+        cb(null, uuidv4() + "-" + file.originalname)
+      }
+})
+
+const fileFilter = (req, file, cb) =>{
+    if(file.mimetype === 'image/png' || file.mimetype === 'image/jpg' || file.mimetype === 'image/jpeg'){
+        cb(null, true)
+    }
+    else{
+        cb(null, false)
+    }
+}
+
 //app.use(bodyParser.urlencoded()); //x-www-form-url-encoded <form>
 app.use(bodyParser.json()) //application/json
+
+app.use(multer({storage: fileStorage, fileFilter: fileFilter}).single('image'))
 
 //make image folder public
 app.use('/images', express.static(path.join(__dirname,'images')))
@@ -28,6 +50,17 @@ app.use((req,res, next) => {
 });
 
 app.use('/feed', feedRoutes)
+
+app.use('/auth', authRoutes)
+
+app.use((error, req, res, next) =>{
+    console.log(error);
+    const status= error.statusCode || 500
+    const message = error.message;
+    const data = error.data
+    res.status(status).json({message: message, data: data});
+})
+
 
 mongoose.connect(MONGODB_URI).then(result => {
     app.listen(8080)
